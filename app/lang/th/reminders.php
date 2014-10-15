37d998bc45c667ab46efaa63b57ff9b9aba841eb
Tonsai Singhkangwan <t@iton.in.th>
2014-10-15 10:34:25 +0700

    Password reminder strings translation

---
+++ app/lang/th/reminders.php
@@ -0,0 +1,26 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reminder Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines which match reasons
+	| that are given by the password broker for a password update attempt
+	| has failed, such as for an invalid token or invalid new password.
+	|
+	*/
+
+	"password" => "รหัสผ่านต้องมีความยาวมากกว่า 6 ตัวอักษร และต้องตรงกับการยืนยัน",
+
+	"user" => "ไม่พบบัญชีผู้ใช้งานจากอีเมล์ที่กรอก",
+
+	"token" => "Token สำหรับการรีเซ็ตรหัสผ่านนี้ไม่ถูกต้อง",
+
+	"sent" => "Password reminder sent!",
+
+	"reset" => "รหัสผ่านถูกรีเซ็ตแล้ว",
+
+);

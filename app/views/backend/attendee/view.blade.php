12b6ab731a1b9844c6b7a28578037020cf0f5f37
Tonsai Singhkangwan <t@iton.in.th>
2014-12-03 19:28:51 +0700

    Escape all value filled by user

--- app/views/backend/attendee/view.blade.php
+++ app/views/backend/attendee/view.blade.php
@@ -6,32 +6,32 @@
 	<div class="col-md-7">
 		<div class="box">
 			<div class="page-header">
-				<h2>Attendee Info <small>(#{{str_pad($attendee->id, 6, 0, STR_PAD_LEFT)}})</small></h2>
+				<h2>Attendee Info <small>(#{{{str_pad($attendee->id, 6, 0, STR_PAD_LEFT)}}})</small></h2>
 			</div>
 			<div class="row">
 				<div class="col-sm-5">
 					<span class="field-name">ชื่อ</span>
-					<div class="field-display">{{$attendee->prefix.$attendee->name}}</div>
+					<div class="field-display">{{{$attendee->prefix.$attendee->name}}}</div>
 				</div>
 				<div class="col-sm-7">
 					<span class="field-name">นามสกุล</span>
-					<div class="field-display">{{$attendee->surname}}</div>
+					<div class="field-display">{{{$attendee->surname}}}</div>
 				</div>
 			</div>
 			<div class="row">
 				<div class="col-sm-5">
 					<span class="field-name">ชื่อเล่น</span>
-					<div class="field-display">{{$attendee->nickname}}</div>
+					<div class="field-display">{{{$attendee->nickname}}}</div>
 				</div>
 				<div class="col-sm-7">
 					<span class="field-name">E-mail</span>
-					<div class="field-display">{{$attendee->email}}</div>
+					<div class="field-display">{{{$attendee->email}}}</div>
 				</div>
 			</div>
 			<div class="row">
 				<div class="col-sm-9">
 					<span class="field-name">โรงเรียน</span>
-					<div class="field-display">{{$attendee->school}}</div>
+					<div class="field-display">{{{$attendee->school}}}</div>
 				</div>
 				<div class="col-sm-3">
 					<span class="field-name">ชั้น</span>
@@ -43,7 +43,7 @@
 						@elseif ($attendee->class == 0)
 							-
 						@else
-							ม.{{$attendee->class}}
+							ม.{{{$attendee->class}}}
 						@endif
 					</div>
 				</div>
@@ -62,7 +62,7 @@
 			</div>
 			<div class="row">
 				<div class="col-xs-12">
-					<div class="well">{{$attendee->expect}}</div>
+					<div class="well">{{{$attendee->expect}}}</div>
 				</div>
 			</div><br>
 			<div class="page-header">
@@ -131,15 +131,15 @@
 				<table class="table table-bordered">
 					<tr>
 						<td><strong>Health Cond.</strong></td>
-						<td>{{$attendee->health_condition or "<span class='text-muted'><i>None</i></span>"}}</td>
+						<td>{{{$attendee->health_condition or "<span class='text-muted'><i>None</i></span>"}}}</td>
 					</tr>
 					<tr>
 						<td><strong>Food</strong></td>
-						<td>{{$attendee->food_allergy or "<span class='text-muted'><i>None</i></span>"}}</td>
+						<td>{{{$attendee->food_allergy or "<span class='text-muted'><i>None</i></span>"}}}</td>
 					</tr>
 					<tr>
 						<td><strong>Med.</strong></td>
-						<td>{{$attendee->med_allergy or "<span class='text-muted'><i>None</i></span>"}}</td>
+						<td>{{{$attendee->med_allergy or "<span class='text-muted'><i>None</i></span>"}}}</td>
 					</tr>
 				</table>
 			</div>
@@ -153,11 +153,11 @@
 			<div class="row">
 				<div class="col-xs-12">
 					<span class="field-name">เบอร์โทร</span>
-					<div class="field-display">{{$attendee->tel}}</div>
+					<div class="field-display">{{{$attendee->tel}}}</div>
 				</div>
 				<div class="col-xs-12">
 					<span class="field-name">เบอร์ผู้ปกครอง</span>
-					<div class="field-display">{{$attendee->parent_tel}}</div>
+					<div class="field-display">{{{$attendee->parent_tel}}}</div>
 				</div>
 				<div class="col-xs-12">
 					<span class="field-name">การเดินทาง (มา)</span>
@@ -171,15 +171,15 @@
 								   					 6 => 'รถประจำทาง',
 								   					 7 => 'รถสองแถว',
 								   					 8 => 'อื่นๆ') ?>
-						{{$trip[$attendee->trip]}}
+						{{{$trip[$attendee->trip]}}}
 					</div>
 				</div>
 				<div class="col-xs-12">
 					<span class="field-name">การเดินทาง (กลับ)</span>
-					<div class="field-display">{{$trip[$attendee->return_trip]}}</div>
+					<div class="field-display">{{{$trip[$attendee->return_trip]}}}</div>
 				</div>
 				<div class="col-xs-12">
-					<a href="https://fb.com/{{$attendee->facebook_url}}" class="btn btn-sm btn-block btn-default"><i class="glyphicon glyphicon-link"></i> Facebook Profile</a>
+					<a href="https://fb.com/{{{$attendee->facebook_url}}}" class="btn btn-sm btn-block btn-default"><i class="glyphicon glyphicon-link"></i> Facebook Profile</a>
 				</div>
 			</div><br>
 			<div class="page-header">
@@ -187,7 +187,7 @@
 			</div>
 			<div class="text-center">
 				{{ Form::open(array('route' => array('backend.attendee.destroy', $attendee->id), 'method' => 'delete')) }}
-				<a href="{{URL::to('/backend/attendee/'.$attendee->id.'/edit')}}" class="btn btn-sm btn-default"><i class="glyphicon glyphicon-pencil"></i> Edit</a>
+				<a href="{{{URL::to('/backend/attendee/'.$attendee->id.'/edit')}}}" class="btn btn-sm btn-default"><i class="glyphicon glyphicon-pencil"></i> Edit</a>
 				<button type="submit" class="btn btn-sm btn-danger"><i class="glyphicon glyphicon-trash"></i> Delete</submit>
 				{{ Form::close() }}
 			</div>

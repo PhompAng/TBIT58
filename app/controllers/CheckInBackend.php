070be801ea8da6e170977d2fe45af174879f1590
Tonsai Singhkangwan <t@iton.in.th>
2014-12-23 20:03:03 +0700

    Add ability to assign attendee to a room before checked in

--- app/controllers/CheckInBackend.php
+++ app/controllers/CheckInBackend.php
@@ -9,7 +9,7 @@ class CheckInBackend extends \BaseController {
 	 */
 	public function index()
 	{
-		$data = array('attendees' => Attendee::all(), );
+		$data = array('attendees' => Attendee::paginate(30));
 		return View::make('backend.checkin.list', $data);
 	}
 
@@ -68,26 +68,40 @@ class CheckInBackend extends \BaseController {
 	 */
 	public function update($id)
 	{
-		if (Input::has('id')) {
+		if (!Input::has('id')) {
+			return Redirect::to('/backend/checkin/');
+		} else {
 			$id = Input::get('id');
+			$attendee = Attendee::find($id);
+			if (!Input::has('room') && $attendee->room == null) {
+				return Redirect::to('/backend/checkin/')->withErrors(["This attendee wasn't assign to any room!"])->withInput();
+			}
 		}
-		$attending = Attendee::find($id)->attend;
+		$attending = $attendee->attend;
 		$today = date('Y-m-d');
+		$mode = 0;
 		if ($today == '2015-01-11') {
 			$attending->day_1_check = !$attending->day_1_check;
+			$mode = $attending->day_1_check;
 		} elseif ($today == '2015-01-11') {
 			$attending->day_2_check = !$attending->day_2_check;
+			$mode = $attending->day_1_check;
 		} elseif ($today == '2015-01-24') {
 			$attending->day_3_check = !$attending->day_3_check;
+			$mode = $attending->day_1_check;
 		} elseif ($today == '2015-01-25') {
 			$attending->day_4_check = !$attending->day_4_check;
+			$mode = $attending->day_1_check;
 		} elseif ($today == '2015-01-31') {
 			$attending->day_5_check = !$attending->day_5_check;
+			$mode = $attending->day_1_check;
 		} elseif ($today == '2015-02-01') {
 			$attending->day_6_check = !$attending->day_6_check;
+			$mode = $attending->day_1_check;
 		}
 		$attending->save();
-		return Redirect::to('/backend/checkin/')->with('attendee', Attendee::find($id));
+		$attendee->fill(Input::all())->save();
+		return Redirect::to('/backend/checkin/')->with('attendee', Attendee::find($id))->with('mode', $mode);
 	}
 
 
--- app/views/backend/checkin/list.blade.php
+++ app/views/backend/checkin/list.blade.php
@@ -15,23 +15,48 @@
 				<h1>Check-In System</h1>
 			</div>
 			<div class="row">
-				<div class="col-md-5 col-md-offset-3">
-						@if (Session::has('attendee'))
+				@if (Session::has('attendee'))
+					<div class="col-xs-12">
 						<div class="alert alert-info">
-							{{ Session::get('attendee')->prefix.Session::get('attendee')->name." ".Session::get('attendee')->surname." (".Session::get('attendee')->nickname.")" }}
+							{{ Session::get('attendee')->prefix.Session::get('attendee')->name." ".Session::get('attendee')->surname." (".Session::get('attendee')->nickname.") : ห้องเรียน ".Session::get('attendee')->room." | Status : ".Session::get('mode') }}
 							<a href="#" class="pull-right">ยกเลิก</a>
 						</div>
-						@endif
-					{{Form::open(array('url' => '/backend/checkin/update', 'method' => 'PUT'))}}
+					</div>
+				@endif
+				@if ($errors->any())
+					<div class="col-xs-12">
+						<div class="alert alert-danger">{{ $errors->first() }}</div>
+					</div>
+				@endif
+				{{Form::open(array('url' => '/backend/checkin/update', 'method' => 'PUT'))}}
+				<div class="col-md-7">
+					<div class="table-responsive">
+						<table class="table table-bordered">
+							<thead>
+								<th class="text-center">223 (0/90 คน)</th>
+								<th class="text-center">328 (0/50 คน)</th>
+								<th class="text-center">329 (0/150 คน)</th>
+								<th class="text-center">335 (0/90 คน)</th>
+							</thead>
+							<tbody>
+								<tr>
+									<td class="text-center">{{Form::radio('room', 223)}}</td>
+									<td class="text-center">{{Form::radio('room', 328)}}</td>
+									<td class="text-center">{{Form::radio('room', 329)}}</td>
+									<td class="text-center">{{Form::radio('room', 335)}}</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+				<div class="col-md-5">
 						<div class="form-group">
 							{{Form::text('id', null, array('class' => 'form-control text-center', 'placeholder' => 'Attendee\'s ID'))}}
 						</div>
 						{{Form::submit('Check In', array('class' => 'btn btn-primary btn-block btn-sm'))}}
-					{{Form::close()}}
 				</div>
+				{{Form::close()}}
 			</div>
-		</div>
-
 		</div>
 	</div>
 	<div class="col-xs-12">
@@ -48,8 +73,8 @@
 						<th>D4</th>
 						<th>D5</th>
 						<th>D6</th>
+						<th>Room</th>
 						<th>Registered on</th>
-						<th>Actions</th>
 					</thead>
 					<tbody>
 						@foreach ($attendees as $attendee)
@@ -99,29 +124,20 @@
 									<i class="text-muted glyphicon glyphicon-remove">
 								@endif
 							</td>
-							<td>{{$attendee->created_at}}</td>
 							<td>
-								{{Form::open(array('route' => array('backend.checkin.update', $attendee->id), 'method' => 'PUT'))}}
-								<button type="submit" class="btn btn-xs btn-warning"><i class="glyphicon glyphicon-refresh"></i> Toggle</button>
-								{{Form::close()}}
+								{{ $attendee->room or "-" }}
 							</td>
+							<td>{{$attendee->created_at}}</td>
 						</tr>
 						@endforeach
 					</tbody>
 				</table>
 			</div>
+			<div class="text-center">
+				{{ $attendees->links() }}
+			</div>
 		</div>
 	</div>
 </div>
-
-@stop
-
-@section('js')
-
-{{ HTML::script('assets/js/jquery.dataTables.js') }}
-{{ HTML::script('assets/js/dataTables.bootstrap.js') }}
-<script>
-	$('.table').dataTable();
-</script>
 
 @stop
